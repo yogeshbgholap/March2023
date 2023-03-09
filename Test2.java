a9faea4387259b54155340eef15e6f95570ec732
yogeshbgholap <yogesh.bgholap@gmail.com>
2023-03-09 15:10:09 +0530

    QA2 first commit

---
+++ Test2.java
@@ -0,0 +1,16 @@
+package gitPkg;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+	int a =10;
+	int b=30;
+
+	int c=a+b;
+        int d=a*b;
+	System.out.println(c*d);
+	
+	}
+
+}
